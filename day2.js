e3f1c6df7bc2454fb112d306415ddf417a84a820
Zeljko Stevanovic <zsteva@gmail.com>
2024-12-16 20:02:34 +0100

    fix day2 formating

--- day2.js
+++ day2.js
@@ -2,11 +2,11 @@
 -- part 1
 
 function isSafe(row) {
-	if (row.isIncrease() || row.isDecrease()) {
-		return row.diff2().abs().filter((a) => (a > 3)).length == 0;
-	} else {
-		return false;
-	}
+    if (row.isIncrease() || row.isDecrease()) {
+        return row.diff2().abs().filter((a) => (a > 3)).length == 0;
+    } else {
+        return false;
+    }
 }
 
 getInput().splitNL().splitCOL().nums().map((row) => isSafe(row)).filter((a) => a).length;
@@ -16,12 +16,12 @@ getInput().splitNL().splitCOL().nums().map((row) => isSafe(row)).filter((a) => a
 -- part 2
 
 function isSafeRemove1(row) {
-	for (var i = 0; i < row.length; i++) {
+    for (var i = 0; i < row.length; i++) {
         if (isSafe(row.removeN(i))) {
             return true;
         }
     }
-	return false;
+    return false;
 }
 
 getInput().splitNL().splitCOL().nums().map((row) => isSafeRemove1(row)).filter((a) => a).length;
